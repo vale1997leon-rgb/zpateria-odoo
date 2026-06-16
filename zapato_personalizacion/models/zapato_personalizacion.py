9548c21939c1867909ca2246b7b4610b5d308bd7
Juan Valenzuela <vesteban595@gmail.com>
2026-06-16 04:18:24 +0000

    Cambios de mejora many2one con el modulo de zapatos_extension

--- zapato_personalizacion/models/zapato_personalizacion.py
+++ zapato_personalizacion/models/zapato_personalizacion.py
@@ -7,11 +7,11 @@ class ZapatoPersonalizacion(models.Model):
     name = fields.Char(string='Nombre de la Personalización', required=True)
     description = fields.Text(string='Descripción')
     precio = fields.Float(string='Precio Adicional', required=True)
-    zapato_id = fields.Many2one('zapatos.zapato', string='Zapato Base', required=True)
-    color = fields.Char(string='Color Personalizado')
-    material = fields.Char(string='Material Personalizado')
-    talla = fields.Char(string='Tamaño / Horma Especial')
     imagen = fields.Binary(string="Diseño de Referencia")
+    zapato_id = fields.Many2one('zapatos.zapato', string='Zapato Base', required=True)
+    color = fields.Char(string='Color Original', related='zapato_id.color', readonly=True)
+    material = fields.Char(string='Material Original', related='zapato_id.material', readonly=True)
+    talla = fields.Integer(string='Talla Original', related='zapato_id.talla', readonly=True)
     calificacion = fields.Selection([
         ('0', 'Sin Calificar'),
         ('1', 'Muy Bajo'),
@@ -19,4 +19,4 @@ class ZapatoPersonalizacion(models.Model):
         ('3', 'Medio'),
         ('4', 'Alto'),
         ('5', 'Excelente')
-    ], string="Calidad / Prioridad", default='0')
+    ], string="Calidad", default='0')
